84f89ab8b0288ec621c4b21f6d810f73b5b024c9
Emerick Grimm <emerickgrimm@Emericks-MacBook-Air.local>
2024-12-21 10:00:23 +0200

    fixed .env path issue

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
